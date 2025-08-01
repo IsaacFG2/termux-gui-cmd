0e85ba1959d136e0b7d8fb64120564fbf90ead0c
IsaacFG2 <147211323+IsaacFG2@users.noreply.github.com>
2025-08-01 14:10:24 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,9 @@
 #!/data/data/com.termux/files/usr/bin/bash
 cd ~/
-pkg install wget -y
 pkg install x11-repo -y
 pkg install termux-x11-nightly -y
 pkg install xfce4 -y
-cd /data/data/com.termux/files/usr
+cd ../
 curl https://raw.githubusercontent.com/IsaacFG2/termux-gui-cmd/refs/heads/main/gui > gui
 mv gui ./bin
 chmod +rwx ./bin/gui
